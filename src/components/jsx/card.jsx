71c1c659ef5449263a8682b4fd18334e2b1c4e79
ShandilyaHarsh <shandilyaharsh2001@gmail.com>
2021-04-26 15:05:09 +0530

    cards added

--- src/components/jsx/card.jsx
+++ src/components/jsx/card.jsx
@@ -8,11 +8,11 @@ function Cards(props)
 {
 
     return(
-        <div className="container sm-w:200 sm-h:250">
+        <div className="container m-5">
   <img src={props.imgsrc} alt="Avatar" className="image rounded-xl "/>
   <div className="overlay  rounded-xl ">
     <div className="texti">{props.imtxt}</div>
-    <Link to="/curr" class="bg-transparent  py-2 px-4 border btn rounded">
+    <Link to="/curr" class="bg-transparent py-2 px-4 border btn rounded">
   View Achievements
 </Link>
 
--- src/components/jsx/home.jsx
+++ src/components/jsx/home.jsx
@@ -10,7 +10,7 @@ function home(){
     return(<div>
     <Navbar/>
     <p> this is home</p>
-    <div className="flex flex-col md:flex-row ">
+    <div className="flex flex-col md:flex-row  ">
     <Cards imgsrc={Sports} imtxt={"FOR THE LOVE OF IT"}/>
     <Cards imgsrc={Acads} imtxt={"FOR THE RUSH OF IT"}/>
     </div>
